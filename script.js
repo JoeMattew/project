acc37f24c934887fdca261168ebf350e155e59a7
JoeMattew <marat4568731@gmail.com>
2022-10-09 11:07:27 +0600

    moviesDB

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+const numberOfFilms = +prompt ('Skolko filmov vi uje posmotreli?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Kakoy film vi posmotreli posledniy?', ''),
+      b = prompt ('Na skolko vi ego ocenite?', ''),
+      c = prompt ('Kakoy film vi posmotreli posledniy?', ''),
+      d = prompt ('Na skolko vi ego ocenite?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
